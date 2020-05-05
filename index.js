fa6ee6040c96ea1b729f669783cde03b48d97121
Carlo Jude Alfonso <carlojude.alfonso@gmail.com>
2020-05-05 19:50:56 +0800

    Configured body-parser

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 require("dotenv").config();
 require("./db/mongoose");
 
@@ -13,6 +14,8 @@ const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser({ limit: "5mb" }));
 
 const PORT = process.env.PORT;
 
